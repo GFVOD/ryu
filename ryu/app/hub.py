0d591d04904716e2bdd50dcf2be7f74d0931e5b0
Miles <miles962464@gmail.com>
2017-12-04 14:21:36 +0800

    complete the hub test

--- ryu/app/hub.py
+++ ryu/app/hub.py
@@ -6,9 +6,53 @@ from ryu.controller.handler import set_ev_cls
 
 from ryu.ofproto import ofproto_parser
 
-class Hub(app_manager.RyuApp)
+class Hub(app_manager.RyuApp):
     """docstring"""
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self,*args,**kwargs):
         super(Hub,self).__init__(*args,**kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+
+        # install the table-miss flow entry.
+        match = ofp_parser.OFPMatch()
+        actions = [ofp_parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                              ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+
+        # construct flow_mod message and send it.
+        inst = [ofp_parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                                 actions)]
+        mod = ofp_parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        actions = [ofp_parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+        match = ofp_parser.OFPMatch()
+
+        # install flow_mod to avoid packet_in next time.
+        self.add_flow(datapath, 1, match, actions)
+
+        out = ofp_parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,
+            actions=actions)
+        datapath.send_msg(out)
+
+
