b5b49ffdab9efd95934586f749efae398ae2d196
Miles <miles962464@gmail.com>
2017-12-13 22:32:03 +0800

    rewrite from mininet.topo import Topo

---
+++ ryu/app/Simple_DC_Topo.py
@@ -0,0 +1,54 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import irange, dumpNodeConnections
+from mininet.log import setLogLevel
+
+
+class CustomTopo(Topo):
+    "Simple Data Center Topology"
+
+    "linkopts - (1:c1, 2:aggregation, 3: edge) parameters"
+    "fanout - number of child switch per parent switch"
+
+    def __init__(self, linkopts1, linkopts2, linkopts3, fanout=2, **opts):
+        # Initialize topology and default options
+        Topo.__init__(self, **opts)
+
+        # Add your logic here ...
+        self.fanout = fanout
+        core = self.addSwitch('c1')
+        for i in irange(1, fanout):
+            aggregation = self.addSwitch('a%s' % i)
+            self.addLink(core, aggregation, **linkopts1)
+            for j in irange(1, fanout):
+                edge = self.addSwitch('e%s' % (fanout * (i - 1) + j))
+                self.addLink(aggregation, edge, **linkopts2)
+                for k in irange(1, fanout):
+                    host = self.addHost('h%s' % ((fanout * (fanout * (i - 1) + j - 1)) + k))
+                    self.addLink(edge, host, **linkopts3)
+
+
+topos = {'custom': (lambda: CustomTopo())}
+
+
+def simpleTest():
+    "Create and test a simple network"
+    linkopts1 = dict(bw=10, delay='3ms', use_htb=True)
+    linkopts2 = dict(bw=8, delay='4ms', loss=1, max_queue_size=900, )
+    linkopts3 = dict(bw=6, delay='5ms', loss=1, max_queue_size=800)
+    topo = CustomTopo(linkopts1, linkopts2, linkopts3, fanout=2)
+    net = Mininet(topo, host=CPULimitedHost, link=TCLink)
+    net.start()
+    print "Dumping host connections"
+    dumpNodeConnections(net.hosts)
+    print "Testing network connectivity"
+    net.pingAll()
+    net.stop()
+
+
+if __name__ == '__main__':
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    simpleTest()
